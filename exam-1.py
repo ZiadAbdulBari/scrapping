5d0fe7b7549c49d4e86a73f4a85769d6ff11bc66
ZiadAbdulBari <ziadabdulbari01@gmail.com>
2025-02-12 01:47:31 +0600

    exception handle

--- exam-1.py
+++ exam-1.py
@@ -1,3 +1,5 @@
+import sys
+sys.stdout.reconfigure(encoding='utf-8')
 import tkinter as tk
 from tkinter import ttk
 from selenium import webdriver
@@ -5,7 +7,7 @@ from selenium.webdriver.common.by import By
 import time
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
-
+from selenium.common.exceptions import NoSuchElementException
 # Create text field and button
 root = tk.Tk()
 root.geometry("800x600")
@@ -30,39 +32,46 @@ chrome_options = Options()
 chrome_options.add_argument("--disable-cache")
 chrome_options.add_argument("--incognito")
 driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://www.google.com/")
+driver.get("https://www.google.com/?hl=en")
 driver.maximize_window()
 driver.refresh()
 search_box = driver.find_element(By.NAME,"q")
 search_box.send_keys(search_text)
 search_box.send_keys(Keys.RETURN)
-time.sleep(10)
+time.sleep(20)
 
 # search the text
 store_web_links = []
-for page in range(0,3):
-    height = driver.execute_script('return document.body.scrollHeight')
-    for i in range(0,height+1500,30):
-        driver.execute_script(f'window.scrollTo(0,{i});')
-        time.sleep(0.2)
-    web_links = driver.find_elements(By.CSS_SELECTOR, '[jsname="UWckNb"]')
-    time.sleep(5)
-    for j in web_links:
-        link = j.get_attribute('href')
-        store_web_links.append(link)
-    next_btn = driver.find_element(By.XPATH,'//*[@id="pnnext"]')
-    time.sleep(0.5)
-    next_btn.click()
+root = tk.Tk()
+root.geometry("800x600")
+try:
+    for page in range(0,3):
+        height = driver.execute_script('return document.body.scrollHeight')
+        for i in range(0,height+1500,30):
+            driver.execute_script(f'window.scrollTo(0,{i});')
+            time.sleep(0.2)
+        web_links = driver.find_elements(By.CSS_SELECTOR, '[jsname="UWckNb"]')
+        time.sleep(5)
+        for j in web_links:
+            link = j.get_attribute('href')
+            store_web_links.append(link)
+        next_btn = driver.find_element(By.XPATH,'//*[@id="pnnext"]')
+        time.sleep(0.5)
+        next_btn.click()
+    # show the links into the table
+    columns = ("Web Links")
+    table = ttk.Treeview(root, columns=columns, show="headings", height=100)
+    table.heading(0, text='Web Links')
+    table.column(0, width=300)
+    for row in store_web_links:
+        table.insert("", tk.END, values=row)
+    table.pack(pady=20)
+except NoSuchElementException:
+    label = tk.Label(root, text='No data found',font=("Helvetica", 13))
+    label.pack()
+
+root.mainloop()   
 driver.quit()
 
-# show the links into the table
-root = tk.Tk()
-columns = ("Web Links")
-table = ttk.Treeview(root, columns=columns, show="headings", height=100)
-table.heading(0, text='Web Links')
-table.column(0, width=300)
-for row in store_web_links:
-    table.insert("", tk.END, values=row)
-table.pack(pady=20)
-root.mainloop()
+
 
